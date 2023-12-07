bb04b81dc28ff4bb5be24058897f0219812a7b03
Brittany Morris <Brittany.Morris32@gmail.com>
2023-12-07 16:52:27 -0500

    WIP: Started on the events page.

---
+++ src/client/layout/EventsPerCity.jsx
@@ -0,0 +1,22 @@
+
+import React from 'react'
+//import { useGetEventsQuery } from './eventsSlice'
+import { useParams, Link } from 'react-router-dom';
+
+export const listOfEvents = ({}) => {
+    // const {data, isError, isLoading} = useGetCitiesQuery();
+
+    // // handle the case where the data is still loading
+    // if (isLoading){
+    //   return (<p>Loading . . .</p>)
+    // }
+  
+    // // handle the case where there's an error in the API call
+    // if (isError){
+    //   return (<p>Error . . .</p>)
+    // }
+
+    return (
+        <div></div>
+    )
+}
